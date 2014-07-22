e04f8f042700c045bbdb1c03e2d82787f2f78173
Fred6681 <frederic.cesart@yahoo.fr>
2014-07-22 12:52:20 +0200

    Added interactiveQ Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
 extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEQ)
+extern struct cpufreq_governor cpufreq_gov_interactiveq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactiveq)
 #endif
 
 
